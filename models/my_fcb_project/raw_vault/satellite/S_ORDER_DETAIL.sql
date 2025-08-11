f777adcc84438a1e4b87434d376ad6dbeb1a8743
amit.h.kumar.tiwari@accenture.com <amit.h.kumar.tiwari@accenture.com>
2025-08-11 11:29:03 +0000

    created satellite

--- models/my_fcb_project/raw_vault/satellite/S_ORDER_DETAIL.sql
+++ models/my_fcb_project/raw_vault/satellite/S_ORDER_DETAIL.sql
@@ -16,5 +16,26 @@ src_payload:
   - "AMOUNT"
   - "LOAD_RK"
   - "SRC_RK"
+src_extra_not_null_columns:
+  - "ORD_BK"
+src_ldts: "LOAD_RK"
+src_source: "SRC_RK"
+
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{
+    dbtvault.sat(
+        src_pk=metadata_dict["src_pk"],
+        src_hashdiff=metadata_dict["src_hashdiff"],
+        src_payload=metadata_dict["src_payload"],
+        src_extra_columns=metadata_dict["src_extra_not_null_columns"],
+        src_eff=metadata_dict["src_eff"],
+        src_ldts=metadata_dict["src_ldts"],
+        src_source=metadata_dict["src_source"],
+        source_model=metadata_dict["source_model"],
+    )
+}}
  
  
